08bd94694f860d69d4c064941de100b562d7d69b
Rohan rana <rohanrana33042@gmail.com>
2026-03-17 22:24:12 +0530

    feat: implement routing and add user/captain authentication pages

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import UserLogin from "./pages/UserLogin";
+import UserSignup from "./pages/UserSIgnup";
+import CaptainLogin from "./pages/CaptainLogin";
+import CaptainSignup from "./pages/CaptainSignup";
 
 const App = () => {
-  return <div className="text-4xl ">rohan</div>;
+  return (
+    <div>
+      <Routes>
+        {/* Default Route */}
+        <Route path="/" element={<Home />} />
+
+        {/* User */}
+        <Route path="/login" element={<UserLogin />} />
+        <Route path="/signup" element={<UserSignup />} />
+
+        {/* Captain */}
+        <Route path="/captain-login" element={<CaptainLogin />} />
+        <Route path="/captain-signup" element={<CaptainSignup />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
---
+++ Frontend/src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <div className="h-screen w-full flex justify-between flex-col bg-red-400">
+        <img />
+
+        <div className="bg-white">
+          <h2>Get Started with Uber</h2>
+          <button>Continue</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
