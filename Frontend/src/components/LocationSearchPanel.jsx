cda0b5647e0dd9b1f99139d78e8a0b3f448c9c50
Rohan rana <rohanrana33042@gmail.com>
2026-03-25 00:40:55 +0530

    feat: implement LocationSearchPanel component and enhance Home layout with GSAP animations

---
+++ Frontend/src/components/LocationSearchPanel.jsx
@@ -0,0 +1,44 @@
+import { RiMapPinFill } from "@remixicon/react";
+
+const LocationSearchPanel = () => {
+  return (
+    <div className="px-5">
+      <div className="flex items-center justify-start my-4 gap-4">
+        <h2 className="bg-[#eee] w-10 h-10 flex flex-shrink-0 items-center justify-center rounded-full">
+          <RiMapPinFill size={20} />
+        </h2>
+        <h4 className="font-medium">2458, Sector 71, Mohali, Punjab</h4>
+      </div>
+
+      <div className="flex items-center justify-start my-4 gap-4">
+        <h2 className="bg-[#eee] w-10 h-10 flex flex-shrink-0 items-center justify-center rounded-full">
+          <RiMapPinFill size={20} />
+        </h2>
+        <h4 className="font-medium">118, MG Road, Bengaluru, Karnataka</h4>
+      </div>
+
+      <div className="flex items-center justify-start my-4 gap-4">
+        <h2 className="bg-[#eee] w-10 h-10 flex flex-shrink-0 items-center justify-center rounded-full">
+          <RiMapPinFill size={20} />
+        </h2>
+        <h4 className="font-medium">22B, Park Street, Kolkata, West Bengal</h4>
+      </div>
+
+      <div className="flex items-center justify-start my-4 gap-4">
+        <h2 className="bg-[#eee] w-10 h-10 flex flex-shrink-0 items-center justify-center rounded-full">
+          <RiMapPinFill size={20} />
+        </h2>
+        <h4 className="font-medium">9, Banjara Hills, Hyderabad, Telangana</h4>
+      </div>
+
+      <div className="flex items-center justify-start my-4 gap-4">
+        <h2 className="bg-[#eee] w-10 h-10 flex flex-shrink-0 items-center justify-center rounded-full">
+          <RiMapPinFill size={20} />
+        </h2>
+        <h4 className="font-medium">401, FC Road, Pune, Maharashtra</h4>
+      </div>
+    </div>
+  );
+};
+
+export default LocationSearchPanel;
--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,4 +1,8 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { RiArrowDownWideFill, RiUserFill } from "@remixicon/react";
+import LocationSearchPanel from "../components/LocationSearchPanel";
 
 const Home = () => {
   const [pickup, setPickup] = useState("");
@@ -9,8 +13,25 @@ const Home = () => {
     e.stopPropagation();
   };
 
+  const panelRef = useRef();
+
+  useGSAP(
+    function () {
+      if (panelOpen) {
+        gsap.to(panelRef.current, {
+          height: "70%",
+        });
+      } else {
+        gsap.to(panelRef.current, {
+          height: "0",
+        });
+      }
+    },
+    [panelOpen],
+  );
+
   return (
-    <div className="h-screen relative">
+    <div className="h-screen relative overflow-hidden">
       <img
         className="w-16 absolute left-5 top-5"
         src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/cc/Uber_logo_2018.png/1280px-Uber_logo_2018.png?_=20180913054426"
@@ -26,7 +47,16 @@ const Home = () => {
 
       <div className=" h-screen flex flex-col justify-end absolute top-0 w-full ">
         <div className="h-[30%] bg-white p-5 relative">
+          {panelOpen && (
+            <h5
+              className="absolute top-5 right-5 cursor-pointer"
+              onClick={() => setPanelOpen(false)}
+            >
+              <RiArrowDownWideFill />
+            </h5>
+          )}
           <h4 className="text-2xl font-semibold">Find a trip</h4>
+
           <form onSubmit={(e) => submitHandler(e)}>
             <div className="line absolute h-16 w-[2px] top-[43%] left-[9%]  bg-gray-800 rounded-full"></div>
 
@@ -49,7 +79,81 @@ const Home = () => {
           </form>
         </div>
 
-        <div className="h-[70%] bg-red-500 p-5 hidden"></div>
+        <div ref={panelRef} className=" bg-white  h-0 ">
+          <LocationSearchPanel />
+        </div>
+      </div>
+
+      <div className="fixed w-full z-10 bottom-0 translate-y-full bg-white px-3 py-8">
+        <h2 className="text-2xl font-semibold mb-5">Choose a vehicle</h2>
+        <div className="flex border-2 active:border-black mb-2 rounded-xl w-full p-3  items-center justify-between">
+          <img
+            className="h-16 "
+            src="https://cn-geo1.uber.com/image-proc/crop/resizecrop/udam/format=auto/width=552/height=552/srcb64=aHR0cHM6Ly90Yi1zdGF0aWMudWJlci5jb20vcHJvZC91ZGFtLWFzc2V0cy8zMDUxZTYwMi0xMGJiLTRlNjUtYjEyMi1lMzk0ZDgwYTljNDcucG5n"
+            alt=""
+          />
+          <div className=" w-1/2 ml-2">
+            <h4 className="font-medium text-base w-full ">
+              UberGo{" "}
+              <span className="inline-flex items-center ">
+                <RiUserFill size={15} /> 4
+              </span>
+            </h4>
+
+            <h5 className="font-medium text-sm">2 Mins away</h5>
+            <p className="font-normal text-xs text-gray-600">
+              Affordable, compact rides
+            </p>
+          </div>
+
+          <h1 className="text-lg font-semibold">₹193.20</h1>
+        </div>
+
+        <div className="flex border-2 active:border-black mb-2 rounded-xl w-full p-3  items-center justify-between">
+          <img
+            className="h-14"
+            src="https://cn-geo1.uber.com/image-proc/crop/resizecrop/udam/format=auto/width=552/height=552/srcb64=aHR0cHM6Ly90Yi1zdGF0aWMudWJlci5jb20vcHJvZC91ZGFtLWFzc2V0cy85NTM4NTEyZC1mZGUxLTRmNzMtYmQ1MS05Y2VmZjRlMjU0ZjEucG5n"
+            alt=""
+          />
+          <div className=" w-1/2 ml-2">
+            <h4 className="font-medium text-base w-full ">
+              Moto{" "}
+              <span className="inline-flex items-center ">
+                <RiUserFill size={15} /> 1
+              </span>
+            </h4>
+
+            <h5 className="font-medium text-sm">5 Mins away</h5>
+            <p className="font-normal text-xs text-gray-600">
+              Affordable, motorcycle rides
+            </p>
+          </div>
+
+          <h1 className="text-lg font-semibold">₹83.75</h1>
+        </div>
+
+        <div className="flex border-2 active:border-black mb-2 rounded-xl w-full p-3  items-center justify-between">
+          <img
+            className="h-10"
+            src="https://clipart-library.com/2023/Uber_Auto_312x208_pixels_Mobile.png"
+            alt=""
+          />
+          <div className=" w-1/2 ml-2">
+            <h4 className="font-medium text-base w-full ">
+              UberAuto{" "}
+              <span className="inline-flex items-center ">
+                <RiUserFill size={15} /> 3
+              </span>
+            </h4>
+
+            <h5 className="font-medium text-sm">3 Mins away</h5>
+            <p className="font-normal text-xs text-gray-600">
+              Affordable, compact rides
+            </p>
+          </div>
+
+          <h1 className="text-lg font-semibold">₹133.25</h1>
+        </div>
       </div>
     </div>
   );
