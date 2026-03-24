2b9f43fd9822a34de62ed95435913af41a30d4c3
Rohan rana <rohanrana33042@gmail.com>
2026-03-24 22:54:22 +0530

    feat: add @gsap/react dependency for enhanced animation support

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -16,7 +16,7 @@ const Home = () => {
         src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/cc/Uber_logo_2018.png/1280px-Uber_logo_2018.png?_=20180913054426"
       />
 
-      <div className="h-screen w-screen ">
+      <div className="h-screen w-screen  ">
         <img
           className="h-full w-full object-cover"
           src="https://raw.githubusercontent.com/MindorksOpenSource/Uber-Car-Animation-Android/master/assets/how-to-add-uber-car-animation-in-android-app-draw-path.jpg"
